b8fe376f1093e466b235abd4a3854b97f7df3e49
Luk.Lu <luk.lu@faronear.org>
2018-08-14 19:29:36 +0800

    检查token>0, money>0, token<this.poolBalance

--- server/Ling/Bancor.js
+++ server/Ling/Bancor.js
@@ -21,7 +21,10 @@ MOM._model={ // 数据模型，用来初始化每个对象的数据
   hash:          { default:undefined, sqlite:'TEXT UNIQUE' },
   crr:           { default:1,          sqlite:'NUMERIC',     mysql:'VARCHAR(64) PRIMARY KEY' }, 
   poolBalance:       { default:1,          sqlite:'NUMERIC',     mysql:'INT' },
-  poolSupply:        { default:1,          sqlite:'NUMERIC'}
+  poolSupply:        { default:1,          sqlite:'NUMERIC'},
+  money:             { default:0,  sqlite:'NUMERIC'},
+  token:             { default:0,  sqlite:'NUMERIC'},
+  price:             { default:NaN,  sqlite:'NUMERIC'}
 }
 MOM._tablekey='hash'
 
@@ -31,43 +34,73 @@ MOM.checkPrice=function(){ // 查询当前价格
 
 MOM.buy=async function(option){ // 实际买入价和量
   option=option||{}
-  if (option.money) {
+  if (option.money>0) {
     option.token=this.poolSupply*(Math.pow(1+option.money/this.poolBalance, this.crr)-1) // 花多少钱==>得到多少币
-  }else if (option.token) {
+  }else if (option.token>0) {
     option.money=this.poolBalance*(Math.pow(1+option.token/this.poolSupply, 1/this.crr)-1) // 买多少币==>要花多少钱
+  }else{
+    if (option.usage==='EXEC'){ 
+      return this
+    }else {
+      return {
+        money:0, token:0, price:NaN, poolBalance:this.poolBalance, poolSupply:this.poolSupply
+      }
+    }
   }
   if (option.usage==='EXEC'){ 
     this.money=option.money
     this.token=option.token
+    this.price=this.token/this.money
     this.poolBalance+=option.money
     this.poolSupply+=option.token
 //    await this.addMe()
     return this
   }else { // 只返回计算的结果，用于测试，不改变储备金池、流通总量
-    return { money:option.money, token:option.token, poolBalance:this.poolBalance+option.money, poolSupply:this.poolSupply+option.token }
+    return {
+      money:option.money,
+      token:option.token,
+      price:option.token/option.money,
+      poolBalance:this.poolBalance+option.money,
+      poolSupply:this.poolSupply+option.token
+    }
   }
 }
 
 MOM.sell=async function(option){ // 实际卖出价和量
   option=option||{}
-  if (option.money) {
+  if (option.money>0) {
     option.token=this.poolSupply*(1-Math.pow(1-option.money/this.poolBalance, this.crr)) // 收回多少钱==>卖出多少币
-  }else if (option.token) {
+  }else if (option.token>0 && option.token<this.poolBalance) {
     option.money=this.poolBalance*(1-Math.pow(1-option.token/this.poolSupply, 1/this.crr)) // 卖出多少币==>收回多少钱
+  }else{
+    if (option.usage==='EXEC'){ 
+      return this
+    }else {
+      return {
+        money:0, token:0, price:NaN, poolBalance:this.poolBalance, poolSupply:this.poolSupply
+      }
+    }
   }
   if (option.usage==='EXEC'){ 
     this.money=option.money
     this.token=option.token
+    this.price=this.token/this.money
     this.poolBalance-=option.money
     this.poolSupply-=option.token
 //    await this.addMe()
     return this
-  }else { // 只返回计算的结果，用于测试，不改变储备金池、流通总量
-    return { money:option.money, token:option.token, poolBalance:this.poolBalance-option.money, poolSupply:this.poolSupply-option.token }
+  }else { // 只返回计算的结果，用于查询，不改变储备金池、流通总量
+    return { 
+      money:option.money, 
+      token:option.token, 
+      price:option.token/option.money,
+      poolBalance:this.poolBalance-option.money, 
+      poolSupply:this.poolSupply-option.token 
+    }
   }
 }
 
-DAD.buy=async function(option){
+DAD.buy=async function(option){ // option里要提供一个bancor数模
   option=option||{}
   let bancor=new DAD(option.model)
   return await bancor.buy(option)
@@ -100,4 +133,4 @@ DAD.api.buy=async function(option){
 my={}
 
 my.TIC=new DAD({ crr:0.33, poolSupply:1200000000, poolBalance:1200000 })
-// todo: 从数据库中读入该模型最新的数据
+// todo: 从数据库中读入该币种最新的模型数据
