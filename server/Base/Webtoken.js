3f8ea59be7852c956619eea26b911e0d946a4c08
lulu <lulu@yuanjin.net>
2018-08-24 19:09:21 +0800

    在WebToken.js里，1）对key进行一次hash; 2）支持对普通字符串、数字进行反解，之前的creatToken参数必须是对象或JSON格式字符串才能反解。

--- server/Base/Webtoken.js
+++ server/Base/Webtoken.js
@@ -1,12 +1,17 @@
 const JsonWebToken = require('jsonwebtoken')
 
 const Tool = new (require('./Egg.js'))()
+const Crypto = require('./Crypto.js')
 
 module.exports={
-  createToken: function(content, key) { // content 可以是字符串或对象，不可以是数组。
+  createToken: function(content, key) { // content 可以是数字，字符串或对象，不可以是数组。
     key=key||Tool.readPath('wo.Config.TOKEN_KEY') // key或wo.Config.TOKEN_KEY其中之一必须存在
-    if (content && typeof(key)==='string' && key.length>0){ // 注意，jwt.sign(null|'') 会出错。但 sign(0)可以的。
-      return JsonWebToken.sign(content, key)
+    if (content && !Array.isArray(content) && typeof(key)==='string' && key.length>0){ // 注意，jwt.sign(null|'') 会出错。但 sign(0)可以的。
+      try{
+        return JsonWebToken.sign(content, Crypto.hash(key))
+      }catch(exp){
+        return null
+      }
     }
     return null
   }
@@ -14,12 +19,15 @@ module.exports={
   verifyToken: function(token, key) {
     key=key||Tool.readPath('wo.Config.TOKEN_KEY') // key或wo.Config.TOKEN_KEY其中之一必须存在
     if (token && typeof token==='string' && typeof(key)==='string' && key.length>0) {
-try{  token=JsonWebToken.verify(token, key)  }catch(exp){ return null }
+      try{
+        token=JsonWebToken.verify(token, Crypto.hash(key))
+      }catch(exp){
+        return null
+      }
       if (Date.now() - Date.parse(token.whenStamp) > 2*60*60*1000) { // 每过2小时，核对一遍密码
 
       }
-      if (typeof token==='object')
-        return token
+      return token
     }
     return null
   }
